4029ed5971dc60efd08ed5fa1f0bd8dffcaf15ff
meet6070 <Meet@6070>
2026-06-04 16:02:09 +0530

    - deployment update

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const memberRoutes = require('./routes/memberRoutes');
 const passwordPublicRoutes = require('./routes/passwordPublicRoutes');
 const globalErrorHandler = require('./middlewares/globalErrorHandler');
 const requestTimeout = require('./middlewares/requestTimeout');
+
 const {
   getRequestTimeoutMs,
   getWebhookRequestTimeoutMs,
